d8f12259e4f4e7969a1f4de539c3d661cb5aae31
Shubham-Singh1810 <hittheshubham1810@gmail.com>
2025-01-27 12:05:27 +0530

    after adding my booking list api

--- src/controller/bookingController.js
+++ src/controller/bookingController.js
@@ -13,11 +13,30 @@ bookingController.post("/create",  async (req, res) => {
     sendResponse(res, 200, "Success", {
       message: "Booking created successfully!",
       data: bookingCreated,
+      statusCode:200
     });
   } catch (error) {
     console.error(error);
     sendResponse(res, 500, "Failed", {
       message: error.message || "Internal server error",
+      statusCode:500
+    });
+  }
+});
+bookingController.get("/my-list/:id",  async (req, res) => {
+  try {
+    const {id} = req.params
+    const bookingList = await booking.find({userId:id});
+    sendResponse(res, 200, "Success", {
+      message: "Booking list retrived successfully!",
+      data: bookingList,
+      statusCode:200
+    });
+  } catch (error) {
+    console.error(error);
+    sendResponse(res, 500, "Failed", {
+      message: error.message || "Internal server error",
+      statusCode:500
     });
   }
 });
