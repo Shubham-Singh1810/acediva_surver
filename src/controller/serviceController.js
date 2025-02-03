d538867e61acc4cc10b95a92aced601ef08795c2
Shubham-Singh1810 <hittheshubham1810@gmail.com>
2025-02-03 12:39:51 +0530

    after adding search key api

--- src/controller/serviceController.js
+++ src/controller/serviceController.js
@@ -187,5 +187,39 @@ serviceController.post("/details", async (req, res) => {
     });
   }
 });
+serviceController.post("/search-list", async (req, res) => {
+  try {
+    const { searchKey = "", status, pageNo = 1, pageCount = 10, sortByField, sortByOrder } = req.body;
+
+    const query = {};
+    if (status) query.status = status;
+    if (searchKey) query.name = { $regex: searchKey, $options: "i" };
+
+    // Construct sorting object
+    const sortField = sortByField || "createdAt";
+    const sortOrder = sortByOrder === "asc" ? 1 : -1;
+    const sortOption = { [sortField]: sortOrder };
+
+    // Fetch the category list
+    const serviceList = await service
+      .find(query)
+      .sort(sortOption)
+      .limit(parseInt(pageCount))
+      .skip(parseInt(pageNo - 1) * parseInt(pageCount));
+
+    const totalCount = await service.countDocuments({});
+    const activeCount = await service.countDocuments({ status: true });
+    sendResponse(res, 200, "Success", {
+      message: "Service list retrieved successfully!",
+      documentCount: { totalCount, activeCount, inactiveCount: totalCount - activeCount },
+      data: serviceList,
+    });
+  } catch (error) {
+    console.error(error);
+    sendResponse(res, 500, "Failed", {
+      message: error.message || "Internal server error",
+    });
+  }
+});
 
 module.exports = serviceController;
